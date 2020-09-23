4a282bfbacdf2907f9e5ffc15385e780f11c9004
RHUL-James-King <H1tchH1k3r!>
2020-09-23 19:02:35 +0100

    Refactor Innings, add further Innings testing

--- app/src/test/java/com/jcking97/umpirecricket/InningsTest.kt
+++ app/src/test/java/com/jcking97/umpirecricket/InningsTest.kt
@@ -24,4 +24,63 @@ class InningsTest {
         assertEquals(0, innings.getOverCount())
     }
 
+    @Test
+    fun whenBallsBowledThenCurrentBallIncreasedWhenBallsBowledReachesLimitThenNewOver() {
+        for (over in 1..3) {
+            for (ball in 1..5) {
+                innings.ballBowled()
+                assertEquals(ball, innings.getCurrentBall())
+                assertEquals(6, innings.getBallLimit())
+                assertEquals(over-1, innings.getOverCount())
+            }
+            innings.ballBowled()
+            assertEquals(0, innings.getCurrentBall())
+            assertEquals(6, innings.getBallLimit())
+            assertEquals(over, innings.getOverCount())
+        }
+    }
+
+    @Test
+    fun whenExtraBallsThenBallLimitIncreasedAndCurrentBallIncreasedAndOverNotIncreased() {
+        for (ball in 1..12) {
+            innings.extraBall()
+            assertEquals(ball, innings.getCurrentBall())
+            assertEquals(ball+6, innings.getBallLimit())
+            assertEquals(0, innings.getOverCount())
+        }
+    }
+
+    @Test
+    fun whenExtraBallsThenBallLimitIncreasedWhenBallsBowledThenCurrentBallIncreasedAndOverNotIncreasedUntilLimitReached() {
+        val extraBalls: Int = 6
+        for (over in 1..3) {
+            for (extraBall in 1..extraBalls) {
+                innings.extraBall()
+                assertEquals(extraBall, innings.getCurrentBall())
+                assertEquals(extraBall+6, innings.getBallLimit())
+                assertEquals(over-1, innings.getOverCount())
+            }
+            for (ball in 1..5) {
+                innings.ballBowled()
+                assertEquals(extraBalls+ball, innings.getCurrentBall())
+                assertEquals(extraBalls+6, innings.getBallLimit())
+                assertEquals(over-1, innings.getOverCount())
+            }
+            innings.ballBowled()
+            assertEquals(0, innings.getCurrentBall())
+            assertEquals(6, innings.getBallLimit())
+            assertEquals(over, innings.getOverCount())
+        }
+    }
+
+    @Test
+    fun whenEndOverThenOversIncreased() {
+        for (over in 1..12) {
+            innings.endOver()
+            assertEquals(0, innings.getCurrentBall())
+            assertEquals(6, innings.getBallLimit())
+            assertEquals(over, innings.getOverCount())
+        }
+    }
+
 }
